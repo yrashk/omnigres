63bef44b3d33a9614eb65613a0d47ab43eaf0324
Diogo Biazus <diogo@biazus.ca>
2024-12-26 15:32:36 -0500

    Test case with mandatory parameters

--- extensions/omni_rest/src/postgrest_rpc.sql
+++ extensions/omni_rest/src/postgrest_rpc.sql
@@ -1,3 +1,20 @@
+create or replace function _postgrest_function_call_arguments (fn regproc, passed_arguments text[])
+    returns text immutable
+    language sql
+    as $$
+    select
+        string_agg(format('%1$I => $%2$s :: %3$I', name, idx, t.typname), ', ')
+    from
+        pg_proc p,
+        unnest(proargnames, proargtypes, proargmodes)
+    with ordinality as _ (name, type, mode, idx)
+    join pg_type t on t.oid = type
+where
+    type is not null
+    and p.oid = fn
+    and name::text = any (passed_arguments)
+$$;
+
 create procedure postgrest_rpc (request omni_httpd.http_request, outcome inout omni_httpd.http_outcome, settings postgrest_settings default postgrest_settings ())
 language plpgsql
 as $$
@@ -6,6 +23,9 @@ declare
     query text;
     namespace text;
     result jsonb;
+    arguments_definition text;
+    argument_values jsonb;
+    passed_arguments text[];
 begin
     if outcome is distinct from null then
         return;
@@ -24,14 +44,52 @@ begin
         return;
         -- terminate;
     end if;
-    query := format('select %1$s() as result', function_reference);
+    select
+        case request.method
+        when 'GET' then
+        (
+            select
+                array_agg(name)
+            from
+                unnest(omni_web.parse_query_string (request.query_string))
+                with ordinality as _ (name, i)
+            where
+                i % 2 = 1)
+        when 'POST' then
+        (
+            select
+                array_agg(jsonb_object_keys)
+            from
+                jsonb_object_keys(convert_from(request.body, 'utf-8')::jsonb))
+        end into passed_arguments;
+    arguments_definition := coalesce(omni_rest._postgrest_function_call_arguments (function_reference, passed_arguments), '');
+    query := format('select %1$s(%2$s) as result', function_reference, arguments_definition);
+    select
+        case request.method
+        when 'GET' then
+        (
+            select
+                jsonb_agg(value::text)
+            from
+                unnest(omni_web.parse_query_string (request.query_string))
+                with ordinality as _ (value, i)
+            where
+                i % 2 = 0)
+        when 'POST' then
+        (
+            select
+                jsonb_agg(v)
+            from
+                jsonb_each_text(convert_from(request.body, 'utf-8')::jsonb) as _ (k,
+                    v))
+        end into argument_values;
     -- Run it
     declare message text;
     detail text;
     hint text;
     begin
         select
-            omni_sql.execute (query) -> 'result' into result;
+            omni_sql.execute (query, coalesce(argument_values, '[]'::jsonb)) -> 'result' into result;
         outcome := omni_httpd.http_response (result);
     exception
         when others then
@@ -41,6 +99,5 @@ begin
     outcome := omni_httpd.http_response (jsonb_build_object('message', message, 'detail', detail, 'hint', hint), status => 400);
     end;
 end;
-
 $$;
 
